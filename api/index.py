d4f98f65433fc39ad6ee2125ae9c98aa702b69a6
Luqman240 <110061089+Luqman240@users.noreply.github.com>
2023-11-17 11:20:02 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 
+"""
 # Load your pre-trained model
 #model = pickle.load(open('nba-result-ml.pkl', 'rb'))
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -14,6 +15,7 @@ file_path = os.path.join(current_directory, 'nba-result-ml.pkl')
 
 with open(file_path, 'rb') as f:
     model = pickle.load(f)
+"""
     
 @app.route('/favicon.ico') 
 def favicon(): 
@@ -23,6 +25,7 @@ def favicon():
 def hello_world():
     return 'This is the NBA Result Prediction API!'
 
+"""
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -37,6 +40,6 @@ def predict():
 
     except Exception as e:
         return jsonify({'error': str(e)})
-
+"""
 if __name__ == '__main__':
     app.run(debug=True)
